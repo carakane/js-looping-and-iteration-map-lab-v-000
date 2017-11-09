c1c6f8399e2d284d9bed36d82945dd9d1d5da1c4
Cara Kane <cara.kane@gmail.com>
2017-11-09 18:26:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function lowerCaseDrivers(drivers) {
 
 function nameToAttributes(drivers) {
   console.log(drivers[0].split(" ")[0])
-  return drivers.map(driver => Object.assign{{firstName: driver.split(" ")[0]}, {lastName: driver.split(" ")[1]}})
+  return drivers.map(driver => {firstName: driver.split(" ")[0]}, {lastName: driver.split(" ")[1]})
 }
 
 function attributesToPhrase() {
